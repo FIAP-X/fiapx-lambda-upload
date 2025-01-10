5a40141667e24919aba58cb5fd6e42e85b177103
DeividCezar <perspicilum@gmail.com>
2025-01-10 08:57:46 -0300

    Teste unitário e exception

--- app/src/lambda_function.py
+++ app/src/lambda_function.py
@@ -1,27 +1,31 @@
+import os
 import boto3
 from botocore.config import Config
 
 def lambda_handler(event, context):
+    try:
+        region_name = os.environ['REGION_NAME']
+        access_key_id = os.environ['LAMBDA_AWS_ACCESS_KEY_ID']
+        secret_access_key = os.environ['LAMBDA_AWS_SECRET_ACCESS_KEY']
+        session_token = os.environ.get('LAMBDA_AWS_SESSION_TOKEN', None)
+        bucket_name = os.environ['BUCKET_NAME']
 
-    region_name = os.environ['REGION_NAME']
-    access_key_id = os.environ['LAMBDA_AWS_ACCESS_KEY_ID']
-    secret_access_key = os.environ['LAMBDA_AWS_SECRET_ACCESS_KEY']
-    session_token = os.environ.get('LAMBDA_AWS_SESSION_TOKEN', None)
-    bucket_name = os.environ['BUCKET_NAME']
+        s3_client = boto3.client(
+            's3',
+            region_name=region_name,
+            aws_access_key_id=access_key_id,
+            aws_secret_access_key=secret_access_key,
+            aws_session_token=session_token,
+            config=Config(signature_version="s3v4")
+        )
 
-    s3_client = boto3.client(
-        's3',
-        region_name = region_name,
-        aws_access_key_id = access_key_id,
-        aws_secret_access_key = secret_access_key,
-        aws_session_token = session_token,
-        config = Config(signature_version = "s3v4")
-    )
+        url = s3_client.generate_presigned_url(
+            ClientMethod='put_object',
+            Params={'Bucket': bucket_name, 'Key': 'videos/video.mkv'},
+            ExpiresIn=60,
+        )
 
-    url = s3_client.generate_presigned_url(
-        ClientMethod='put_object',
-        Params={'Bucket': bucket_name, 'Key': 'videos/video.mkv',},
-        ExpiresIn=60,
-    )
+        return {"statusCode": 200, "body": url}
 
-    return {"statusCode": 200, "body": url}
+    except Exception as e:
+        return {"statusCode": 500, "body": str(e)}
--- app/tests/test_lambda_function.py
+++ app/tests/test_lambda_function.py
@@ -1,9 +1,29 @@
-import unittest
+import os
+from unittest import TestCase, mock
+from lambda_function import lambda_handler
 
-class TestEmpty(unittest.TestCase):
+class TestLambdaHandler(TestCase):
 
-    def test_empty(self):
-        print("Testado")
+    @mock.patch('lambda_function.boto3.client')
+    def test_lambda_handler_success(self, mock_boto3_client):
+        os.environ['REGION_NAME'] = 'us-east-1'
+        os.environ['LAMBDA_AWS_ACCESS_KEY_ID'] = 'fake_access_key'
+        os.environ['LAMBDA_AWS_SECRET_ACCESS_KEY'] = 'fake_secret_key'
+        os.environ['BUCKET_NAME'] = 'test-bucket'
+        mock_s3_client = mock.Mock()
+        mock_boto3_client.return_value = mock_s3_client
+        mock_s3_client.generate_presigned_url.return_value = 'http://example.com'
+        response = lambda_handler({}, None)
+        self.assertEqual(response['statusCode'], 200)
+        self.assertEqual(response['body'], 'http://example.com')
 
-if __name__ == '__main__':
-    unittest.main()
+    @mock.patch('lambda_function.boto3.client')
+    def test_lambda_handler_error(self, mock_boto3_client):
+        os.environ['REGION_NAME'] = 'us-east-1'
+        os.environ['LAMBDA_AWS_ACCESS_KEY_ID'] = 'fake_access_key'
+        os.environ['LAMBDA_AWS_SECRET_ACCESS_KEY'] = 'fake_secret_key'
+        os.environ['BUCKET_NAME'] = 'test-bucket'
+        mock_boto3_client.side_effect = Exception("Erro simulado ao conectar com o S3")
+        response = lambda_handler({}, None)
+        self.assertEqual(response['statusCode'], 500)
+        self.assertIn("Erro simulado ao conectar com o S3", response['body'])
