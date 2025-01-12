142313888f2d412c9492c963cab3a7472114a395
DeividCezar <perspicilum@gmail.com>
2025-01-12 15:13:21 -0300

    Criando integração com o gateway

--- infra/main.tf
+++ infra/main.tf
@@ -15,3 +15,37 @@ resource "aws_lambda_function" "lambda_upload" {
     }
   }
 }
+
+resource "aws_api_gateway_resource" "lambda_resource" {
+  rest_api_id = var.api_gateway_id
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  path_part   = "generate-url"
+}
+
+resource "aws_api_gateway_method" "lambda_method" {
+  rest_api_id   = var.api_gateway_id
+  resource_id   = aws_api_gateway_resource.lambda_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = var.api_gateway_id
+  resource_id             = aws_api_gateway_resource.lambda_resource.id
+  http_method             = aws_api_gateway_method.lambda_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.my_lambda.arn}/invocations"
+}
+
+resource "aws_lambda_permission" "allow_api_gateway" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.my_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+}
+
+resource "aws_api_gateway_deployment" "api_deployment" {
+  rest_api_id = var.api_gateway_id
+  stage_name  = "prod"
+}
