88a821967f6ec51fc2378e0479bba2aca450daf2
DeividCezar <perspicilum@gmail.com>
2025-01-19 09:04:05 -0300

    Requirements.txt

--- app/tests/test_lambda_function.py
+++ app/tests/test_lambda_function.py
@@ -2,13 +2,14 @@ import os
 import json
 import unittest
 from unittest.mock import patch, MagicMock
+import uuid
 from lambda_function import lambda_handler
 
 class TestLambdaHandler(unittest.TestCase):
 
     @patch('lambda_function.boto3.client')
-    def test_lambda_handler_success(self, mock_boto_client):
-
+    @patch('lambda_function.uuid.uuid4', return_value=uuid.UUID('12345678-1234-5678-1234-567812345678'))
+    def test_lambda_handler_success(self, mock_uuid, mock_boto_client):
         os.environ['REGION_NAME'] = 'us-east-1'
         os.environ['LAMBDA_AWS_ACCESS_KEY_ID'] = 'fake-access-key'
         os.environ['LAMBDA_AWS_SECRET_ACCESS_KEY'] = 'fake-secret-key'
@@ -36,7 +37,8 @@ class TestLambdaHandler(unittest.TestCase):
         self.assertEqual(json.loads(response['body'])['url'], "https://fakeurl.com")
         mock_s3_client.generate_presigned_url.assert_called_once_with(
             ClientMethod='put_object',
-            Params={'Bucket': 'fake-bucket-name', 'Key': 'videos/user123/'}
+            Params={'Bucket': 'fake-bucket-name', 'Key': 'videos/user123/12345678-1234-5678-1234-567812345678'},
+            ExpiresIn=3600
         )
 
     @patch('lambda_function.boto3.client')
